fb5604a6f0bfe0aaaa0ea0fa19a08989389ef20f
feoalexei <feoalexei@ukr.net>
2023-01-19 08:14:26 +0200

    added "Delete" option

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import TodoList from './TodoList';
+import initialTodos from '../todos.json';
+
+class App extends Component {
+  state = {
+    todos: initialTodos,
+  };
+
+  deleteTodo = todoId => {
+    this.setState(prevState => ({
+      todos: prevState.todos.filter(todo => todo.id !== todoId),
+    }));
+  };
+
+  render() {
+    const { todos } = this.state;
+    const totalTodosCount = todos.length;
+    const completedTodosCount = todos.reduce(
+      (acc, todo) => (todo.completed ? acc + 1 : acc),
+      0
+    );
+
+    return (
+      <>
+        <div>
+          <span>Total number of todos: {totalTodosCount}</span>
+          <br></br>
+          <span>Number of completed todos: {completedTodosCount}</span>
+        </div>
+        <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />
+      </>
+    );
+  }
+}
+
+export default App;
